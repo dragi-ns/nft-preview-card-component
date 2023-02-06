03cb59c55d750cebe5b58a78935dd95c38324653
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2023-02-06 20:34:48 +0100

    Add nft card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-blue": "hsl(215, 51%, 70%)",
+        cyan: "hsl(178, 100%, 50%)",
+        "main-very-dark-blue": "hsl(217, 54%, 11%)",
+        "card-very-dark-blue": "hsl(216, 50%, 16%)",
+        "line-very-dark-blue": "hsl(215, 32%, 27%)",
+      },
+      backgroundImage: {
+        "image-overlay": "url('../images/icon-view.svg')",
+      },
+    },
   },
   plugins: [],
 };
